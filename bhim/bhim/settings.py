9c24997068d7a07e980ea41a9b8dfc531aa6d8b8
vandana <vandanainsa1993@gmail.com>
2022-01-30 13:47:24 +0530

    second commit

--- bhim/bhim/settings.py
+++ bhim/bhim/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -31,13 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login',
+
 
 ]
 
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'bhim.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +84,7 @@ DATABASES = {
         'NAME': 'DB3',
         'USER':'postgres',
         'PASSWORD':'Armaan@123',
-        'HOST':'localhost'
+        'HOST':'localhost',
     }
 }
 
@@ -117,15 +120,17 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+MEDIA_ROOT=(BASE_DIR/'media')
+MEDIA_URL='media/'
 
+STATICFILES_DIRS=[
+    (BASE_DIR/'bhim/static')]
+STATIC_ROOT=(BASE_DIR/'static')
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    (BASE_DIR,'static')
-]
-STATIC_ROOT=(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- bhim/bhim/urls.py
+++ bhim/bhim/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+import login.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls')),
-    path('login/', include('login.urls')),
-    path('signup/', include('signup.urls')),
+    path('',login.views.home,name='home')
+    ]
 
 
-]
